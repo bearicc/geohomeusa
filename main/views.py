44915bdb475499ae81b304665d6d9c59566fbfbd
bearicc <hxiongustc@gmail.com>
2015-08-11 12:31:27 -0500

    .

--- main/views.py
+++ main/views.py
@@ -20,6 +20,9 @@ def home(request):
     # weixin url validate
     signature = request.GET.get('signature', '')
     echostr   = request.GET.get('echostr', '')
+    with open('tmp', 'w') as f:
+        print(signature)
+        f.write(signature)
     if signature:
         if validateURL(signature):
             return HttpResponse(echostr)
