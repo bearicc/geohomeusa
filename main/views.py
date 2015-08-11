9dd6724ff33cb3610db96af5fc2dbc63c43adc1b
bearicc <hxiongustc@gmail.com>
2015-08-11 11:46:06 -0500

    .

--- main/views.py
+++ main/views.py
@@ -13,10 +13,10 @@ from lib import random_string
 
 def home(request):
     # weixin url validate
-    signature = request.GET.get('signature')
-    timestamp = request.GET.get('timestamp')
-    nonce     = request.GET.get('nonce')
-    echostr   = request.GET.get('echostr')
+    signature = request.GET.get('signature', '')
+    timestamp = request.GET.get('timestamp', '')
+    nonce     = request.GET.get('nonce', '')
+    echostr   = request.GET.get('echostr', '')
     if signature:# and validateURL(signature):
         return echostr
 
