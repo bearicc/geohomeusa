7309bcf2776d4a688f9780666b0bea5a6bec2f46
bearicc <hxiongustc@gmail.com>
2015-08-11 15:43:52 -0500

    .

--- main/views.py
+++ main/views.py
@@ -4,29 +4,30 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login as login_, logout as logout_
 from django.contrib.auth.decorators import login_required
-from uuid import uuid4
+# from uuid import uuid4
 # from urllib import parse
 # from urllib.parse import parse_qs
 import requests
 import requests.auth
 # from copy import deepcopy
-from lib import random_string
+# from lib import random_string
 from django.conf import settings
 import os
 from wechat_sdk import WechatBasic
 
 WXAPPID = 'wxc6f432c17d775275'
 WXAPPSECRET = '912c6962607e7a409d93a9ee0a5cabae'
-WXTOKEN = '12345'
+WXTOKEN = 'abcdef'
 
 
 def home(request):
     # weixin url validate
     token = WXTOKEN
     signature = request.GET.get('signature', '')
-    # echostr = request.GET.get('echostr', '')
+    echostr = request.GET.get('echostr', '')
     timestamp = request.GET.get('timestamp', '')
     nonce = request.GET.get('nonce')
+    return HttpResponse(echostr)
     if signature:
         debug_log('signature: '+signature)
         return weixin_response(token, signature, timestamp, nonce)
@@ -38,6 +39,7 @@ def home(request):
             return HttpResponse('')
     """
 
+    """
     user = None
     user_info = None
     if request.user.is_authenticated():
@@ -70,8 +72,10 @@ def home(request):
                 user.is_authenticated = True
                 user.backend = 'django.contrib.auth.backends.ModelBackend'
                 login_(request, user)
+    """
 
-    return render(request, 'index.html', {'user': user, 'user_info': user_info})
+    return HttpResponse('Interacting with weixin ...')
+    # return render(request, 'index.html', {'user': user, 'user_info': user_info})
 
 
 def aboutus(request):
@@ -200,4 +204,39 @@ def debug_log(string, mode='a'):
 
 
 def weixin_response(token, signature, timestamp, nonce):
-	return HttpResponse('OK')
+    # 用户的请求内容 (Request 中的 Body)
+    # 请更改 body_text 的内容来测试下面代码的执行情况
+    body_text = """
+    <xml>
+    <ToUserName><![CDATA[touser]]></ToUserName>
+    <FromUserName><![CDATA[fromuser]]></FromUserName>
+    <CreateTime>1405994593</CreateTime>
+    <MsgType><![CDATA[text]]></MsgType>
+    <Content><![CDATA[wechat]]></Content>
+    <MsgId>6038700799783131222</MsgId>
+    </xml>
+    """
+
+    # 实例化 wechat
+    wechat = WechatBasic(token=token)
+    # 对签名进行校验
+    if wechat.check_signature(signature=signature, timestamp=timestamp, nonce=nonce):
+        # 对 XML 数据进行解析 (必要, 否则不可执行 response_text, response_image 等操作)
+        wechat.parse_data(body_text)
+        # 获得解析结果, message 为 WechatMessage 对象 (wechat_sdk.messages中定义)
+        message = wechat.get_message()
+
+        response = None
+        if message.type == 'text':
+            if message.content == 'wechat':
+                response = wechat.response_text(u'^_^')
+            else:
+                response = wechat.response_text(u'文字')
+        elif message.type == 'image':
+            response = wechat.response_text(u'图片')
+        else:
+            response = wechat.response_text(u'未知')
+
+        # 现在直接将 response 变量内容直接作为 HTTP Response 响应微信服务器即可，此处为了演示返回内容，直接将响应进行输出
+        return response
+    return HttpResponse('')
